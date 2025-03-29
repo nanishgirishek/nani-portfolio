1d6b0ee369694911095fb1df296f95d8bee062df
nanish girishek <nanishnanish6.com>
2025-03-29 06:19:04 -0700

    added images

---
+++ node_4ecac4c192076.tmp.js
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio | NANISH</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky">
+        <div class="container">
+            <a class="navbar-brand" href="#">Portfolio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#skills">skills</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<div class="row mb-5">
+
+    <div class="container1 col d-flex justify-content-end p5 pe-5 m5 mx-7 my-4 ">
+        <img src="C:\nani portfolio\WhatsApp Image 2025-03-19 at 05.59.32_1b6c1d44.jpg" class="img" alt="Profile Picture">
+        
+    </div> 
+       <div class="container col mx-5 ">
+        <section class="hero text-center">
+            <div class="container">
+                <h1>HELLO It's me</h1>
+                <h2>NANISH</h2>
+                <h3>And I'm a <span class="text-primary" >Frontend Developer</span></h3>
+                <p>"MERN stack developer skilled in building responsive and dynamic web applications with modern technologies."</p>
+                <a href="C:\nani portfolio\resume.girishek-1 (2).docx" class="btn btn-primary">Download CV</a>
+                <div class="social-icons mt-3">
+                    <a href="https://www.linkedin.com/in/giri-shek-886390284" target="_blank">
+                        <i class="fab fa-linkedin fa-2x"></i>
+                    </a>
+                    <a href="https://www.instagram.com/ft_nanxshhh__" target="_blank">
+                        <i class="fab fa-instagram fa-2x"></i>
+                    </a>
+                    <a href="https://www.snapchat.com/add/nani190606?share_id=YwGD85ZQMnQ&locale=en-US" target="_blank">
+                        <i class="fab fa-snapchat fa-2x"></i>
+                    </a>
+                </div>
+                
+            </div>
+        </section>
+    </div>
+    <div class="row " id="about">
+        <div class="container2 col ">
+            <img src="C:/nani portfolio/WhatsApp Image 2025-03-19 at 05.59.32_1b6c1d44.jpg" class="img1 w-50 h-50px mt-5 ms-5 my-5 shadow-lg" alt="Profile Picture">
+        </div>
+        <div class="about col mt-5 ms-1 w-40 ">
+            <h1><span class="text-primary">ABOUT ME</span> </h1>
+            <p class="para">       <br>  I’m a passionate MERN stack developer with a strong foundation in HTML, CSS, JavaScript, React, Bootstrap, and GitHub. I specialize in building dynamic and responsive web applications, ensuring seamless user experiences with clean and efficient code.
+                
+                Currently, I’m exploring opportunities at Google and seeking an internship at Zoho to enhance my skills and contribute to real-world projects. My goal is to create impactful digital solutions by leveraging the power of modern web technologies.
+        </p>
+        </div>
+
+    </div>
+    <div class="row  my-5 p-5" id="skills">
+        <div class="col">
+            <h1 class="h11">My Skills</h1>
+            <section>
+                <div class="myskills" id="skill">
+                    <div class="technicol-bar">
+                        
+                        <div class="bar">
+                            <i class='bx bxl-html5'></i>
+                            <div class="info">
+                                <h3>html</h3>
+                            </div>
+                            <div class="progress-line html">
+                                <span></span>
+                            </div>
+                        </div>
+            
+                        <div class="bar">
+                            <i class='bx bxl-css3'></i>
+                            <div class="info">
+                                <h3>css</h3>
+                            </div>
+                            <div class="progress-line css">
+                                <span></span>
+                            </div>
+                        </div>
+            
+                        <div class="bar">
+                            <i class='bx bxl-javascript'></i>
+                            <div class="info">
+                                <h3>javascript</h3>
+                            </div>
+                            <div class="progress-line javascript">
+                                <span></span>
+                            </div>
+                        </div>
+            
+                        <div class="bar">
+                            <i class='bx bxl-python'></i>
+                            <div class="info">
+                                <h3>Python</h3>
+                            </div>
+                            <div class="progress-line python">
+                                <span></span>
+                            </div>
+                        </div>
+            
+                        <div class="bar">
+                            <i class='bx bxl-react'></i>
+                            <div class="info">
+                                <h3>React</h3>
+                            </div>
+                            <div class="progress-line react">
+                                <span></span>
+                            </div>
+                        </div>
+            
+                    </div>
+                </div>
+            </section>
+
+        </div>
+
+
+
+    </div>
+    
+
+
+
+
+</div>
+<footer id="contact" >
+    <hr>
+    <h3>contact:</h3>
+    <div class="social-icons mt-3">
+        <a href="https://www.linkedin.com/in/giri-shek-886390284" target="_blank">
+            <i class="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="https://www.instagram.com/ft_nanxshhh__" target="_blank">
+            <i class="fab fa-instagram fa-2x"></i>
+        </a>
+        <a href="https://www.snapchat.com/add/nani190606?share_id=YwGD85ZQMnQ&locale=en-US" target="_blank">
+            <i class="fab fa-snapchat fa-2x"></i>
+        </a>
+    </div>
+    <br>
+
+     <h6>nanishnanish6@gmail.com</h4>
+</footer>
+
+
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
+
+</body>
+</html>
